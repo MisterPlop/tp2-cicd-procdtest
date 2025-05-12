3aaa570df68de6f16525e96bb390a29e8e8c026a
Alan <alan.konak@ecoles-epsi.net>
2025-05-12 17:26:10 +0200

    🚨 fix dernière erreur lint

--- test_main.py
+++ test_main.py
@@ -23,5 +23,6 @@ def test_hello_performance():
     duration = time.time() - start
     assert duration < 1
 
+
 def test_hello_full_name():
     assert hello("Jane", "Smith") == "Hello, Jane Smith!"
